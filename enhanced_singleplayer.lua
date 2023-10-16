071d28c241b905a16f1ff585c198fa0cbd209f97
Meadow <124762645+MeadowMidnight@users.noreply.github.com>
2023-10-16 19:23:43 +1100

    adjusted end of heist rewards and made loot pickups scale depending on difficulty

--- enhanced_singleplayer.lua
+++ enhanced_singleplayer.lua
@@ -278,14 +278,82 @@ if RequiredScript == "lib/tweak_data/tweakdata" and Check_Singleplayer() then --
         self.weapon["m4_npc"].alert_size = 500
         self.weapon["mp5_npc"].alert_size = 500
         self.weapon["raging_bull_npc"].alert_size = 500
-        -- Also increases the value of loot to make grinding less tedious --
+    end, false)
+    function TweakData:_set_easy()
+        self.player:_set_easy()
+        self.character:_set_easy()
+        self.group_ai:_set_easy()
+        self.experience_manager.total_level_objectives = 1000
+        self.experience_manager.total_criminals_finished = 200
+        self.experience_manager.total_objectives_finished = 750
+        self.experience_manager.civilians_killed = 50
+        self.experience_manager.values.size03 = 200
+        self.experience_manager.values.size12 = 200
+        self.experience_manager.values.size20 = 400
+        self.experience_manager.values.size16 = 500
+        self.experience_manager.values.size06 = 200
+        self.experience_manager.values.size18 = 400
+    end
+    function TweakData:_set_normal()
+        self.player:_set_normal()
+        self.character:_set_normal()
+        self.group_ai:_set_normal()
+        self.experience_manager.total_level_objectives = 2000
+        self.experience_manager.total_criminals_finished = 400
+        self.experience_manager.total_objectives_finished = 1500
+        self.experience_manager.civilians_killed = 100
+        self.experience_manager.values.size03 = 400
+        self.experience_manager.values.size12 = 400
+        self.experience_manager.values.size20 = 800
+        self.experience_manager.values.size16 = 1000
+        self.experience_manager.values.size06 = 400
+        self.experience_manager.values.size18 = 800
+    end
+    function TweakData:_set_hard()
+        self.player:_set_hard()
+        self.character:_set_hard()
+        self.group_ai:_set_hard()
+        self.experience_manager.total_level_objectives = 3000
+        self.experience_manager.total_criminals_finished = 600
+        self.experience_manager.total_objectives_finished = 2250
+        self.experience_manager.civilians_killed = 150
+        self.experience_manager.values.size03 = 600
+        self.experience_manager.values.size12 = 600
+        self.experience_manager.values.size20 = 1200
+        self.experience_manager.values.size16 = 1500
+        self.experience_manager.values.size06 = 600
+        self.experience_manager.values.size18 = 1200
+    end
+    function TweakData:_set_overkill()
+        self.player:_set_overkill()
+        self.character:_set_overkill()
+        self.group_ai:_set_overkill()
+        self.experience_manager.total_level_objectives = 4000
+        self.experience_manager.total_criminals_finished = 800
+        self.experience_manager.total_objectives_finished = 3000
+        self.experience_manager.civilians_killed = 200
+        self.experience_manager.values.size03 = 800
+        self.experience_manager.values.size12 = 800
+        self.experience_manager.values.size20 = 1600
+        self.experience_manager.values.size16 = 2000
+        self.experience_manager.values.size06 = 800
+        self.experience_manager.values.size18 = 1600
+    end
+    function TweakData:_set_overkill_145()
+        self.player:_set_overkill_145()
+        self.character:_set_overkill_145()
+        self.group_ai:_set_overkill_145()
+        self.experience_manager.total_level_objectives = 6000
+        self.experience_manager.total_criminals_finished = 1200
+        self.experience_manager.total_objectives_finished = 4500
+        self.experience_manager.civilians_killed = 300
         self.experience_manager.values.size03 = 1000
         self.experience_manager.values.size12 = 1000
         self.experience_manager.values.size20 = 2000
         self.experience_manager.values.size16 = 2500
         self.experience_manager.values.size06 = 1000
         self.experience_manager.values.size18 = 2000
-    end, false)
+    end
 end
 if RequiredScript == "lib/tweak_data/playertweakdata" and Check_Singleplayer() then -- Shouting cooldown is reduced to 1 second instead of 1.5 and fixes ADS inconsistency with STRYK.
     local PlayerTweakDataHook = module:hook_class("PlayerTweakData")
