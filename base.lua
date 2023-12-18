359c7e63a2a3c79656834c23277713d9a7826a02
Meadow <124762645+MeadowMidnight@users.noreply.github.com>
2023-12-18 17:48:29 +1100

    Update base.lua

--- base.lua
+++ base.lua
@@ -1,5 +1,5 @@
 local module = DorHUDMod:new("Enhanced Singleplayer", { abbr = "ES",
-	author = "MeadowMidnight", version = "1" description = {
+	author = "MeadowMidnight", version = "1", description = {
 		english = "Adds player-side and team AI enhancements in singleplayer heists whilst avoiding major changes to enemy-side balance. Joining multiplayer lobbies, unlocking achievements and saving steam statistics are disabled while this mod is active."
 	}
 })
